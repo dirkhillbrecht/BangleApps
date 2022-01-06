7b704953995001d8169c73260c1653dc4a37432d
Dirk Hillbrecht (home) <dirk@hillbrecht.de>
2022-01-06 18:55:16 +0100

    Anton clock WS v0.11: Removed in favor for the changes of the official Anton clock

--- apps/antwsclk/antonws.settings.js
+++ apps/antwsclk/antonws.settings.js
@@ -1,4 +1,4 @@
-// Settings menu for the enhanced Anton clock
+// Removed settings menu for the enhanced Anton clock
 
 (function(back) {
   var FILE = "antwsclk.json";
@@ -11,89 +11,24 @@
     require('Storage').writeJSON(FILE, settings);
   }
 
-  // Helper method which uses int-based menu item for set of string values
-  function stringItems(startvalue, writer, values) {
-    return {
-      value: (startvalue === undefined ? 0 : values.indexOf(startvalue)),
-      format: v => values[v],
-      min: 0,
-      max: values.length - 1,
-      wrap: true,
-      step: 1,
-      onchange: v => {
-        writer(values[v]);
-        writeSettings();
-      }
-    };
-  }
-
-  // Helper method which breaks string set settings down to local settings object
-  function stringInSettings(name, values) {
-    return stringItems(settings[name], v => settings[name] = v, values);
-  }
-
   var mainmenu = {
     "": {
-      "title": "Anton clock"
+      "title": "Anton clock WS"
     },
     "< Back": () => back(),
-    "Seconds...": () => E.showMenu(secmenu),
-    "Date": stringInSettings("dateOnMain", ["Short", "Long", "ISO8601"]),
-    "Show Weekday": {
+    "N/A": {
       value: (settings.weekDay !== undefined ? settings.weekDay : true),
       format: v => v ? "On" : "Off",
       onchange: v => {
         settings.weekDay = v;
         writeSettings();
       }
-    },
-    "Uppercase": {
-      value: (settings.upperCase !== undefined ? settings.upperCase : false),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.upperCase = v;
-        writeSettings();
-      }
-    },
-    "Vector font": {
-      value: (settings.vectorFont !== undefined ? settings.vectorFont : false),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.vectorFont = v;
-        writeSettings();
-      }
-    },
-  };
-
-  // Submenu
-  var secmenu = {
-    "": {
-      "title": "Show seconds..."
-    },
-    "< Back": () => E.showMenu(mainmenu),
-    "Show": stringInSettings("secondsMode", ["Never", "Unlocked", "Always"]),
-    "With \":\"": {
-      value: (settings.secondsWithColon !== undefined ? settings.secondsWithColon : false),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.secondsWithColon = v;
-        writeSettings();
-      }
-    },
-    "Color": {
-      value: (settings.secondsColoured !== undefined ? settings.secondsColoured : false),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.secondsColoured = v;
-        writeSettings();
-      }
-    },
-    "Date": stringInSettings("dateOnSecs", ["No", "Year", "Weekday"])
+    }
   };
 
   // Actually display the menu
   E.showMenu(mainmenu);
 
-});
+})(load);
 
 // end of file
